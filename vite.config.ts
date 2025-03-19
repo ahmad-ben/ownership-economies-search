02e5eda63e0b57a1bbb530516769ac147e76ca95
ahmad-ben <ahmedbenchakhter@gmail.com>
2025-03-19 15:56:26 +0000

    Change building details + Rebuild the app.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: "ownership-economies-search", // This is the directory name of the repository
+  build: {
+    outDir: "docs"
+  }
 })
